fa66c6ac85f5ab8daae45d16a5ac0d3321fa0d4f
Dmitry Kulikov <dima@koulikoff.ru>
2019-12-03 18:27:20 +0100

    Annotate

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: col
+#
+#  id     :integer          primary key
+#  crt    :integer          not null
+#  mod    :integer          not null
+#  scm    :integer          not null
+#  ver    :integer          not null
+#  dty    :integer          not null
+#  usn    :integer          not null
+#  ls     :integer          not null
+#  conf   :text             not null
+#  models :text             not null
+#  decks  :text             not null
+#  dconf  :text             not null
+#  tags   :text             not null
+#
+
 require 'rails_helper'
 
 describe Collection, type: :model do
